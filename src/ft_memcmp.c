1aa55b1dff7b1d88df532b5f8eb0ca7b557af691
ttakala <ttakala@student.hive.fi>
2024-02-18 10:46:47 +0200

    optimize ft_memcmp

--- src/ft_memcmp.c
+++ src/ft_memcmp.c
@@ -6,27 +6,37 @@
 /*   By: ttakala <ttakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:55:23 by ttakala           #+#    #+#             */
-/*   Updated: 2023/12/22 14:38:50 by ttakala          ###   ########.fr       */
+/*   Updated: 2024/02/18 10:38:51 by ttakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <stdint.h>
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*uns1;
-	unsigned char	*uns2;
+	const uint64_t	*qw1 = s1;
+	const uint64_t	*qw2 = s2;
+	const uint8_t	*uns1;
+	const uint8_t	*uns2;
 
-	uns1 = (unsigned char *)s1;
-	uns2 = (unsigned char *)s2;
-	while (n-- != 0)
+	while (n >= sizeof(uint64_t))
+	{
+		if (*qw1 != *qw2)
+			break ;
+		qw1++;
+		qw2++;
+		n -= sizeof(uint64_t);
+	}
+	uns1 = (const uint8_t *)qw1;
+	uns2 = (const uint8_t *)qw2;
+	while (n > 0)
 	{
 		if (*uns1 != *uns2)
-		{
 			return ((int)(*uns1 - *uns2));
-		}
 		uns1++;
 		uns2++;
+		n--;
 	}
 	return (0);
 }
