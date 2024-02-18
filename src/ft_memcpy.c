100569a43a4bec310e5978204213d2a4df2b5b45
ttakala <ttakala@student.hive.fi>
2024-02-18 11:14:05 +0200

    fit ft_memcpy within a single function

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -6,44 +6,31 @@
 /*   By: ttakala <ttakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 18:28:23 by ttakala           #+#    #+#             */
-/*   Updated: 2024/02/14 19:13:48 by ttakala          ###   ########.fr       */
+/*   Updated: 2024/02/18 11:08:33 by ttakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-
-static inline void
-	ft_copy_chunks(unsigned char **dst, const unsigned char **src, size_t n)
-{
-	unsigned long		*d;
-	const unsigned long	*s;
-
-	d = (unsigned long *)*dst;
-	s = (const unsigned long *)*src;
-	*dst = *dst + n * sizeof(long);
-	*src = *src + n * sizeof(long);
-	while (n--)
-		*d++ = *s++;
-}
+#include <stdint.h>
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char		*d;
-	const unsigned char	*s;
-	size_t				chunks;
-	unsigned int		trail;
+	const uint64_t	*q_src = (const uint64_t *)src;
+	uint64_t		*q_dst;
+	const uint8_t	*uns_src;
+	uint8_t			*uns_dst;
 
-	if (dst == NULL && src == NULL)
-		return (NULL);
-	if (n == 0 || dst == src)
+	if ((!dst && !src) || n == 0 || dst == src)
 		return (dst);
-	d = (unsigned char *)dst;
-	s = (const unsigned char *)src;
-	chunks = n / sizeof(long);
-	trail = n % sizeof(long);
-	if (chunks)
-		ft_copy_chunks(&d, &s, chunks);
-	while (trail--)
-		*d++ = *s++;
+	q_dst = (uint64_t *)dst;
+	while (n >= sizeof(uint64_t))
+	{
+		*q_dst++ = *q_src++;
+		n -= sizeof(uint64_t);
+	}
+	uns_src = (const uint8_t *)q_src;
+	uns_dst = (uint8_t *)q_dst;
+	while (n--)
+		*uns_dst++ = *uns_src++;
 	return (dst);
 }
